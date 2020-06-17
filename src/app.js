84dab6c4eb0fa7f242367c381741f34bc0c0e793
André Ghisleni Raimann <andre@andreg.com.br>
2020-06-17 13:54:05 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const cors = require("cors");
+
+const { uuid } = require("uuidv4");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+app.get("/repositories", (req, res) => {
+  return res.json(repositories);
+});
+
+app.post("/repositories", (req, res) => {
+  const { title, url, techs } = req.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return res.json(repository);
+});
+
+app.put("/repositories/:id", (req, res) => {
+  const { id } = req.params;
+
+  const { title, url, techs } = req.body;
+
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+  if (repositoryIndex < 0)
+    return res.status(400).json({ error: "Repository not found." });
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositoryIndex].likes,
+  };
+  repositories[repositoryIndex] = repository;
+  return res.json(repository);
+});
+
+app.delete("/repositories/:id", (req, res) => {
+  const { id } = req.params;
+
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+  if (repositoryIndex < 0)
+    return res.status(400).json({ error: "Repository not found." });
+  repositories.splice(repositoryIndex, 1);
+  return res.status(204).send();
+});
+
+app.post("/repositories/:id/like", (req, res) => {
+  const { id } = req.params;
+
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryIndex < 0)
+    return res.status(400).json({ error: "Repository not found." });
+
+  const { likes, ...repo } = repositories[repositoryIndex];
+
+  const repository = {
+    ...repo,
+    likes: likes + 1,
+  };
+  repositories[repositoryIndex] = repository;
+  return res.json(repository);
+});
+
+module.exports = app;
